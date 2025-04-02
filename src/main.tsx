0ed264d33aaf1cc3aa72eff5f0b1dd9ccc3a2eb3
Andrew Allison <andrew.allison@xdesign.com>
2025-04-02 21:20:02 +0100

    feat: add routing debug

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from '@tanstack/react-router';
 import { router } from './router';
+import { TanStackRouterDevtools } from '@tanstack/router-devtools';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RouterProvider router={router} />
+    {import.meta.env.DEV && <TanStackRouterDevtools router={router} />}
   </StrictMode>,
 );
